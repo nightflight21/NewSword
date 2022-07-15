445e064c6738a72645f18b87a2e01cd19eff3388
Mark <markc7856@yahoo.com>
2022-07-15 15:21:55 -0600

    mediocre collision detection script for now

--- Sword/Scripting/CollideActorsAction.cs
+++ Sword/Scripting/CollideActorsAction.cs
@@ -33,6 +33,27 @@ namespace Sword
                 {
                     // resolve by changing the actor's color to something else
                     actor2.Tint(Color.Red());
+                    actor1.SetHealth(actor1.GetHealth() - 1);
+                    Console.WriteLine("You got hit! Your health is now " + actor1.GetHealth());
+                    if (actor1.GetLeft() < actor2.GetRight())
+                    {
+                        actor1.MoveTo(actor1.GetLeft() + 10, actor1.GetTop());
+                    }
+                    else if (actor1.GetRight() > actor2.GetLeft())
+                    {
+                        actor1.MoveTo(actor1.GetRight() - 10, actor1.GetTop());
+                    }
+                    else if (actor1.GetTop() < actor2.GetBottom())
+                    {
+                        actor1.MoveTo(actor1.GetLeft(), actor1.GetTop() + 10);
+                    }
+                    else if (actor1.GetBottom() > actor2.GetTop())
+                    {
+                        actor1.MoveTo(actor1.GetLeft(), actor1.GetBottom() - 10);
+                    }
+                    //actor1.MoveTo(0, -10);
+//this.GetLeft() < other.GetRight() && this.GetRight() > other.GetLeft() && this.GetTop() < other.GetBottom() && this.GetBottom() > other.GetTop()
+
                 }
                 else
                 {
