9f1e7b56168dfa8833a0ea98d9955f91e7dbe922
Mark <markc7856@yahoo.com>
2022-07-15 17:16:43 -0600

    added comments for collision and chase

--- Sword/Scripting/ChaseEnemyAction.cs
+++ Sword/Scripting/ChaseEnemyAction.cs
@@ -22,21 +22,24 @@ namespace Sword
             foreach (Actor enemy in enemies)                                        //for each enemy in the group
             try
             {
-                Actor player = scene.GetFirstActor("player");
-                if(enemy.GetPosition().X < player.GetPosition().X + 200 && enemy.GetPosition().X > player.GetPosition().X - 200 && enemy.GetPosition().Y < player.GetPosition().Y + 200 && enemy.GetPosition().Y > player.GetPosition().Y - 200){
-                    if (enemy.GetCenterX() == player.GetCenterX())
+                Actor player = scene.GetFirstActor("player");                       //grabs the player
+                if(enemy.GetPosition().X < player.GetPosition().X + 200             //if enemy is within 200 pixels of the player
+                && enemy.GetPosition().X > player.GetPosition().X - 200             //if enemy is within 200 pixels of the player
+                && enemy.GetPosition().Y < player.GetPosition().Y + 200             //if enemy is within 200 pixels of the player
+                && enemy.GetPosition().Y > player.GetPosition().Y - 200){           //if enemy is within 200 pixels of the player
+                    if (enemy.GetCenterX() == player.GetCenterX())                  //if enemy has same x axis as player
                     {
-                        enemy.MoveTo(player.GetPosition().X, enemy.GetPosition().Y - ((enemy.GetCenterY() - player.GetCenterY())/Math.Abs(enemy.GetCenterY() - player.GetCenterY())));
+                        enemy.MoveTo(player.GetPosition().X, enemy.GetPosition().Y - ((enemy.GetCenterY() - player.GetCenterY())/Math.Abs(enemy.GetCenterY() - player.GetCenterY())));                      //move enemy up or down
                     }
-                    else if (enemy.GetCenterY() == player.GetCenterY())
+                    else if (enemy.GetCenterY() == player.GetCenterY())             //if enemy has same y axis as player
                     {
-                        enemy.MoveTo(enemy.GetPosition().X - ((enemy.GetCenterX() - player.GetCenterX())/Math.Abs(enemy.GetCenterX() - player.GetCenterX())), player.GetPosition().Y);
+                        enemy.MoveTo(enemy.GetPosition().X - ((enemy.GetCenterX() - player.GetCenterX())/Math.Abs(enemy.GetCenterX() - player.GetCenterX())), player.GetPosition().Y);                    //move enemy left or right
                     }
                     else
-                    enemy.MoveTo(
+                    enemy.MoveTo(                                                   //else enemy just moves towards player
                         enemy.GetPosition().X - ((enemy.GetCenterX() - player.GetCenterX())/Math.Abs(enemy.GetCenterX() - player.GetCenterX())), 
                         enemy.GetPosition().Y - ((enemy.GetCenterY() - player.GetCenterY())/Math.Abs(enemy.GetCenterY() - player.GetCenterY())));
-                }
+                }                                                                   //this is to avoid any division by zero errors
             }
             catch (Exception exception)
             {
